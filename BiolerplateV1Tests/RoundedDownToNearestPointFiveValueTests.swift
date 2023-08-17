b481b8fbb58060585b0a637d9e2f43f027c6818b
Shourob Datta <sourovsuet@gmail.com>
2023-08-17 12:52:19 +0200

    no message

---
+++ BiolerplateV1Tests/RoundedDownToNearestPointFiveValueTests.swift
@@ -0,0 +1,22 @@
+//
+//  RoundedDownToNearestValue.swift
+//  BiolerplateV1Tests
+//
+//  Created by Shourob Datta on 17/8/23.
+//
+
+import Foundation
+
+import XCTest
+@testable import BiolerplateV1
+
+final class RoundedDownToNearestValueTests: XCTestCase {
+
+    func testToggleFavorite() async {
+        
+        let rawValue = 4.30
+        let roundedValue = rawValue.roundedDownToNearestHalf()
+       
+        
+    }
+}
